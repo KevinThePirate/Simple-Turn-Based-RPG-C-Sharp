956362c8180ddbe4cd8fa1d2f959f4f3012d9b5e
Kevin <smithkevin1100@gmail.com>
2024-06-22 18:18:13 +0100

    Initial commit

---
+++ TBRPG/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace TBRPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Unit player = new Unit(100, 20, 12, "Player");
+            Unit enemy = new Unit(75, 10, 7, "Enemy Goblin");
+            Random random = new Random();
+
+            while (!player.IsDead && !enemy.IsDead)
+            {
+                Console.WriteLine(player.UnitName + " HP = " + player.Hp + ". " + enemy.UnitName + " HP = " + enemy.Hp);
+                Console.WriteLine("Player Turn! What will you do?");
+                Console.WriteLine("Press 'a' to attack and 'h' to heal!");
+                string choice = Console.ReadLine();
+
+                if (choice == "a")
+                {
+                    player.Attack(enemy);
+                }
+                else if(choice == "h")
+                {
+                    player.Heal();
+                }
+                else {
+                        Console.WriteLine("Try again!");
+                        Console.WriteLine("Player Turn! What will you do?");
+                        Console.WriteLine("Press 'a' to attack and 'h' to heal!");
+                    }
+
+                if (player.IsDead || enemy.IsDead)
+                {
+                    break;
+                }
+
+                Console.WriteLine(player.UnitName + " HP = " + player.Hp + ". " + enemy.UnitName + " HP = " + enemy.Hp);
+                Console.WriteLine("Enemy Turn!");
+
+                int ran = random.Next(0, 3);
+                if (ran != 0)
+                {
+                    enemy.Attack(player); // Corrected to attack the player
+                }
+                else
+                {
+                    enemy.Heal();
+                }
+            }
+        }
+    }
+}
---
+++ TBRPG/Unit.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TBRPG
+{
+    internal class Unit
+    {
+        private int currentHp;
+        private int maxHp;
+        private int attackPower;
+        private int healPower;
+        private string unitName;
+        private Random random;
+
+        public int Hp {  get { return currentHp; }}
+        public string UnitName { get { return unitName; }}
+        public bool IsDead { get { return currentHp <= 0; } }
+
+        public Unit(int maxHp, int attackPower, int healPower, string unitName) // Changed to public
+        {
+            this.maxHp = maxHp;
+            this.currentHp = maxHp;
+            this.attackPower = attackPower;
+            this.healPower = healPower;
+            this.unitName = unitName;
+            this.random = new Random(); // Initialize Random instance
+        }
+
+        public void Attack(Unit unitToAttack)
+        {
+            double rng = random.NextDouble();
+            rng = rng / 2 + 0.75f;
+            int randDamage = (int)(attackPower * rng);
+            unitToAttack.TakeDamage(randDamage);
+            Console.WriteLine(unitName + " attacks " + unitToAttack.unitName + " and deals " + randDamage + " damage.");
+        }
+
+        public void TakeDamage(int damage)
+        {
+            currentHp -= damage; // Changed to subtract damage from currentHp
+            if (IsDead)
+            {
+                Console.WriteLine(UnitName + " has been defeated");
+            }
+        }
+
+        public void Heal()
+        {
+            double rng = random.NextDouble();
+            rng = rng / 2 + 0.75f;
+            int heal = (int)(rng * healPower);
+            currentHp = heal + currentHp > maxHp ? maxHp : currentHp + heal;
+            Console.WriteLine(UnitName + " heals " + heal);
+        }
+    }
+}
